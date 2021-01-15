a4be4733b5debd4dfa414972c9e2f29c1408bef4
ThomasCOSSY <thomascossy888@gmail.com>
2021-01-15 15:44:48 +0100

    version final + page erreur 404

---
+++ erreur.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>page d'erreur</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/style.css">
+</head>
+
+<body>
+    <div>
+        <h1>Nous n'avons pas trouvez la page, mais bonne nouvelle ! nous avons trouvé Mariah !!!</h1>
+    </div>
+    <div class="d-flex align-items-center justify-content-center m-5">
+        <img src="assets/img/mariah1.jpg" alt="mariah for ever">
+    </div>
+
+    <footer class="bg-light text-center align-items-center justify-content-center p-4">
+        <p class="text-center">Maçonnerie Ocordo © 2021 Tous les droits réserves </p>
+    </footer>
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -98,7 +98,9 @@ echo $aboutUs;
 
 ?>
 
-
+<footer class="bg-light text-center align-items-center justify-content-center p-4">
+        <p class="text-center">Maçonnerie Ocordo © 2021 Tous les droits réserves </p>
+    </footer>
 
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
